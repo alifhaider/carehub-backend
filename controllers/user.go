22d39863033f6f08ffc6e0b48bd456d14cb0c840
alif haider <alifhaider57@gmail.com>
2024-10-09 05:50:23 +0600

    login with token

---
+++ controllers/user.go
@@ -0,0 +1,155 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alifhaider/carehub-backend/database"
+	"github.com/alifhaider/carehub-backend/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func SignUp(c *gin.Context) {
+	// Get the username, email, password and full name from the request body
+	var body struct {
+		Username        string
+		Email           string
+		Password        string
+		ConfirmPassword string
+		FullName        string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse request body"})
+		return
+	}
+
+	// Validate username to be non-empty and unique
+	if body.Username == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username is required"})
+		return
+	}
+
+	// check if the username already exists
+	var existingUser models.User
+	result := database.Db.Where("username = ?", body.Username).First(&existingUser)
+	if result.RowsAffected > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username already exists"})
+		return
+	}
+
+	println(result.RowsAffected, "rows affected")
+
+	if body.Email == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email is required"})
+		return
+	}
+
+	// check if the email already exists
+	result = database.Db.Where("email = ?", body.Email).First(&existingUser)
+	if result.RowsAffected > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists"})
+		return
+	}
+
+	if body.Password == "" || body.ConfirmPassword == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Password is required"})
+		return
+	}
+
+	if body.Password != body.ConfirmPassword {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Passwords do not match"})
+		return
+	}
+
+	// hash the password
+	log.Println("password during registration", body.Password)
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash the password"})
+		return
+	}
+
+	log.Println("passwordHash after hashing", string(passwordHash))
+
+	// Create a new user
+	user := models.User{
+		Username: body.Username,
+		Email:    body.Email,
+		Password: string(passwordHash),
+		FullName: body.FullName,
+	}
+
+	// Save the user
+	result = database.Db.Create(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+
+}
+
+func Login(c *gin.Context) {
+	// Get the username and password from the request body
+	var body struct {
+		Username string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse request body"})
+		return
+	}
+
+	// Validate username and password
+	if body.Username == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username is required"})
+		return
+	}
+
+	if body.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Password is required"})
+		return
+	}
+
+	// Get the user from the database
+	var user models.User
+	database.Db.First(&user, "username = ?", body.Username)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username"})
+		return
+	}
+
+	log.Println("passwordHash from db", user.Password)
+	log.Println("password from body", body.Password)
+
+	// Validate the password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		log.Println("Password comparison failed: ", err)
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
+		return
+	}
+
+	// Generate a JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+
+}
